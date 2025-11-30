a8bad1455365776fb061acc0776ef355260f43cd
Magecachaman <165098790+Magecachaman@users.noreply.github.com>
2025-11-30 19:29:25 +0000

    Add files via upload

--- Actividad-LND/js/scripts.js
+++ Actividad-LND/js/scripts.js
@@ -13,25 +13,43 @@ function hidePhoto() {
 
 const try_div = document.getElementById("main-container-1");
 
-try_div.addEventListener("mouseover", showBiology);
-try_div.addEventListener("mouseout", hideBiology);
+// try_div.addEventListener("mouseover", showBiology);
+// try_div.addEventListener("mouseout", hideBiology);
 
-function showBiology() {
-  let showDiv = document.getElementById("monster-ecology")
-  if (try_div.event = "mouseover") {
-    showDiv.style.display = "block";
+// function showBiology() {
+//   let showDiv = document.getElementById("monster-ecology")
+//   if (try_div.event = "mouseover") {
+//     showDiv.style.display = "block";
 
-    console.log(showDiv);
-  } else {
-    showDiv.style.display = "none";
+//     console.log(showDiv);
+//   } else {
+//     showDiv.style.display = "none";
+//   }
+// }
+
+// function hideBiology() {
+//   let showDiv = document.getElementById("monster-ecology")
+//   if (try_div.event = "mouseover") {
+//     showDiv.style.display = "none";
+//   } else {
+//     showDiv.style.display = "block";
+//   }
+// }
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
   }
 }
 
-function hideBiology() {
-  let showDiv = document.getElementById("monster-ecology")
-  if (try_div.event = "mouseover") {
-    showDiv.style.display = "none";
-  } else {
-    showDiv.style.display = "block";
-  }
-}
+window.addEventListener("scroll", reveal, {passive: true});
